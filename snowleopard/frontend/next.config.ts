0d11645123627c829cab49390162ca981ea58058
Trolleroof <nikhilprabhu06@gmail.com>
2025-12-12 15:54:59 -0800

    removing eslint

--- snowleopard/frontend/next.config.ts
+++ snowleopard/frontend/next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Disable ESLint during builds
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // Also ignore TypeScript errors during builds (optional)
     ignoreBuildErrors: true,
